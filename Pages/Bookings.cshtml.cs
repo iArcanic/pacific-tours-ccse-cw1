a8969b30428585b864e63c5ebdf87e764774304d
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-06 13:50:07 +0000

    Added UI for Bookings page and navigation for authenticated users

---
+++ Pages/Bookings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_net_core_web_app_authentication_authorisation.Pages
+{
+    public class BookingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
