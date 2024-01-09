741e3b1973125b114a0322e221e243e4ad42563b
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-09 21:29:19 +0000

    Added Hotel listing and tab persistence on form submit

--- Pages/Bookings.cshtml.cs
+++ Pages/Bookings.cshtml.cs
@@ -1,12 +1,85 @@
+using asp_net_core_web_app_authentication_authorisation.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace asp_net_core_web_app_authentication_authorisation.Pages
 {
     public class BookingsModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public BookingsModel(ApplicationDbContext dbContext)
+        { 
+            Input = new InputModel();
+            _dbContext = dbContext;
+        }
+
+        public class InputModel
         {
+            [Required(ErrorMessage = "Please select a check-in date")]
+            [DataType(DataType.DateTime)]
+            [Display(Name = "Check in date")]
+            public DateTime CheckInDate { get; set; }
+
+            [Required(ErrorMessage = "Please select a check-out date")]
+            [DataType(DataType.DateTime)]
+            [Display(Name = "Check out date")]
+            public DateTime CheckOutDate { get; set; }
+
+            [Required(ErrorMessage = "Please select a room type")]
+            [DataType(DataType.Text)]
+            [Display(Name = "Room type")]
+            public string RoomType { get; set; }
+
+            public string HotelActiveTab { get; set; } = "HotelsTabSelected";
+            public string TourActiveTab { get; set; } = "ToursTabSelected";
+            public string PackageActiveTab { get; set; } = "PackagesTabSelected";
+
+            public List<SelectListItem> RoomTypes { get; set; } = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Value = "single",
+                    Text = "Single"
+                },
+                new SelectListItem
+                {
+                    Value = "double",
+                    Text = "Double"
+                },
+                new SelectListItem
+                {
+                    Value = "family suite",
+                    Text = "Family Suite"
+                }
+            };
+
+            public List<String> AvailableHotels { get; set; } = new List<string>();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var availableHotels = await _dbContext.HotelAvailabilities
+                .Where(ha => 
+                    ha.AvailableFrom <= Input.CheckInDate && ha.AvailableTo >= Input.CheckOutDate)
+                .Select(ha => ha.Hotel.Name)
+                .Distinct()
+                .ToListAsync();
+
+            Input.AvailableHotels = availableHotels;
+
+            return Page();
         }
     }
 }
