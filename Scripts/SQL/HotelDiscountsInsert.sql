b31ac22ad0b971df35a79fd1054d24cdd3234f1f
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-17 19:02:16 +0000

    Added hotel discount insert sql file

---
+++ Scripts/SQL/HotelDiscountsInsert.sql
@@ -0,0 +1,5 @@
+INSERT INTO HotelDiscounts (HotelDiscountId, RoomType, HotelDiscountPercentage)
+VALUES
+    (NEWID(), 'Single', 10.0),
+    (NEWID(), 'Double', 20.0),
+    (NEWID(), 'Family Suite', 40.0);
